a3359150e71bfefb402bc835aaa6858fc2b724f5
aniket-patil7075 <akp7075@gmail.com>
2025-01-24 16:21:08 +0530

    Changes

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ require('./config/database.js').connectDatabase();
 
 app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
-
+app.use(cors());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
